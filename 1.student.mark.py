1db1f94f112db3f55ab0c02cc8e76f97e6fb0a8b
unknown <theduy08012003@gmail.com>
2023-02-24 13:20:01 +0700

    Practice work 1

---
+++ 1.student.mark.py
@@ -0,0 +1,9 @@
+STUDENTS = {}
+COURSE = {}
+
+def STUDENT_input():
+    STUDENT_num = (input("Enter the number of students: "))
+    for i in range (STUDENT_num):
+        STUDENT = {}
+        STUDENT[name] = (input("Enter student's name: "))
+
